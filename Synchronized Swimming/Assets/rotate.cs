f7490587b9a441f3a231dbb29ecf38fc0eb6b1d6
alexalex9999 <alexohsnap@bellsouth.net>
2017-09-26 17:02:14 -0400

    New Project
    Its better

--- Synchronized Swimming/Assets/rotate.cs
+++ Synchronized Swimming/Assets/rotate.cs
@@ -1,15 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
+
 using UnityEngine;
 
 public class rotate : MonoBehaviour {
 
+	int timeHeld= 0;
+	int speedNo = 0;
+	float speedMore = 0; 
+	bool slowDown = false;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.forward * 50f * Time.deltaTime);
+		//if(Input.GetKeyUp(KeyCode.LeftArrow))
+		//	timeHeld = 0;
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			timeHeld++;
+			transform.Rotate (Vector3.forward * (float) Math.Sqrt(timeHeld)*100 * (Time.deltaTime));
+			slowDown = true;
+		}
+
+		if ((Input.GetKeyUp (KeyCode.LeftArrow))||!(Input.GetKey (KeyCode.LeftArrow))) {
+
+			if (slowDown == true) {
+				speedNo = 0;
+				transform.Rotate (Vector3.forward * (float) Math.Sqrt(timeHeld)*100 * (Time.deltaTime));
+				timeHeld -= 2;
+				speedMore = timeHeld * 100;
+				Debug.Log ("SLOW DOWN");
+				if (timeHeld < 0 || timeHeld == 0) {
+					slowDown = false;
+					timeHeld = 0;
+				}
+
+			}
+		}
 	}
 }
