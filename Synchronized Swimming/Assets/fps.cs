952a278ace568c6ee24027417d3553c0efa94721
alexalex9999 <alexohsnap@bellsouth.net>
2017-09-26 13:19:59 -0400

    Rotating sprite

---
+++ Synchronized Swimming/Assets/fps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+
+public class NewBehaviourScript1 : MonoBehaviour {
+    Text fpsText;
+    		float fps = 0;
+    int currentscore;
+	// Use this for initialization
+	void Start () {
+        		fpsText = GetComponent<Text>();
+		fpsText.text= ("" +fps);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fps = (1/Time.deltaTime);
+		fpsText.text= ("" + fps);  
+		Debug.Log ("" + fps);
+
+	}
+}
--- Synchronized Swimming/Assets/rotate.cs
+++ Synchronized Swimming/Assets/rotate.cs
@@ -6,11 +6,10 @@ public class rotate : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Rotate(Vector3.forward * 50f * Time.deltaTime);
 	}
 }
