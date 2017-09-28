49f8ea3d3d93c7e8e40fe678bbd873bf24a2b987
alexalex9999 <alexohsnap@bellsouth.net>
2017-09-28 00:32:49 -0400

    stuff

---
+++ Sync or Sink/Sync or Sink/Assets/rotate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+	int timeHeld= 0;
+	float speedMore = 0; 
+	bool slowDown = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if(Input.GetKeyUp(KeyCode.LeftArrow))
+		//	timeHeld = 0;
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			timeHeld++;
+			transform.Rotate (Vector3.forward * (float) Math.Sqrt(timeHeld)*100 * (Time.deltaTime));
+			slowDown = true;
+		}
+
+		if ((Input.GetKeyUp (KeyCode.LeftArrow))||!(Input.GetKey (KeyCode.LeftArrow))) {
+
+			if (slowDown == true) {
+				transform.Rotate (Vector3.forward * (float) Math.Sqrt(timeHeld)*100 * (Time.deltaTime));
+				timeHeld -= 2;
+				speedMore = timeHeld * 100;
+				Debug.Log ("SLOW DOWN");
+				if (timeHeld < 0 || timeHeld == 0) {
+					slowDown = false;
+					timeHeld = 0;
+				}
+
+			}
+		}
+	}
+}
